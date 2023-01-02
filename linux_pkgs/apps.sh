1ad794ed13c58e95c9c0331a707109b5d17df438
Cameron Rodriguez <dev@camrod.me>
2023-01-01 20:01:11 -0500

    Better automation, use pastel instead of gcolor3

--- linux_pkgs/apps.sh
+++ linux_pkgs/apps.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-# Other apps I use, run from this directory
+# Run from this directory
+if [[ ! "$(dirname $(pwd))" =~ "/linux_pkgs" ]]; then
+    echo "Please run this script from within the \`linux_pkgs\` directory"
+    exit 1
+fi
 
-sudo dnf install dconf-editor duplicity ffmpeg gnome-extensions-app openrgb steam virt-manager zoom
+# Other apps I use
+sudo dnf -y install dconf-editor duplicity ffmpeg gnome-extensions-app openrgb steam virt-manager zoom
 
 # gsettings modifications for RK87 keyboard and dev tool shortcuts
 gsettings set org.gnome.desktop.wm.keybindings activate-window-menu "['<Shift><Super>F10']"
@@ -10,22 +15,18 @@ gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys mic-mute "['AudioStop']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys stop-static "['']"
 
-# Google Chrome
-sudo dnf config-manager --set-enabled google-chrome
-sudo dnf check-update
-
 # ExpressVPN (see _expressvpn-upgrade.py)
 cp ./*expressvpn* ~/scripts # Convenient script to upgrade expressvpn via CLI
 sudo chmod ug+x ./*expressvpn*
 expressvpn-upgrade --install
 
 # vnStat for cool network info
-sudo dnf install vnstat
+sudo dnf -y install vnstat
 sudo systemctl enable vnstat
 sudo systemctl start vnstat
 
 # Flatpaks (slight brace expansion abuse)
-sudo flatpak install \
+sudo flatpak install --noninteractive \
     cc.arduino.IDE2 \
     com.authy.Authy \
     com.calibre_ebook.calibre \
@@ -37,7 +38,6 @@ sudo flatpak install \
     io.github.Qalculate \
     io.github.trigg.discover_overlay \
     md.obsidian.Obsidian \
-    nl.hjdskes.gcolor3 \
     org.gimp.GIMP{,.Plugin.{BIMP,Fourier,Lensfun,LiquidRescale,Resynthesizer}} \
     org.gnome.Evolution \
     org.prismlauncher.PrismLauncher \
@@ -45,6 +45,12 @@ sudo flatpak install \
     tech.feliciano.pocket-casts
 
 sudo flatpak override --env=TERM=xterm-256color --env=LC_MONETARY=en_CA.UTF-8 io.github.Qalculate
+
+# Cargo (Rust)
+yes | cargo install pastel
+sudo appstreamcli put ./pastel.metainfo.xml
+cp ./pastel.desktop ~/.local/share/applications/pastel.desktop
+cp ./pastel-256.png ~/.local/share/icons/hicolor/256x256/apps/pastel.png
 
 # Setup Evolution toolbar
 mkdir -p ~/.var/app/org.gnome.Evolution/config/evolution/ui
--- linux_pkgs/devtools.sh
+++ linux_pkgs/devtools.sh
@@ -1,43 +1,55 @@
 #!/bin/bash
 # Run from this directory
+if [[ ! "$(dirname $(pwd))" =~ "/linux_pkgs" ]]; then
+    echo "Please run this script from within the \`linux_pkgs\` directory"
+    exit 1
+fi
 
-# RPM Fusion, other nonfree libraries
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install fedora-workstation-repositories
+# RPM Fusion, other nonfree libraries, and first updates
+sudo dnf -y check-update
+sudo dnf -y upgrade
+sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y install fedora-workstation-repositories
 
-# Java, C/C++, NodeJS, Perl
-sudo dnf install java-latest-openjdk-devel cmake meson binutils libtool glibc-devel \
+# Java, C/C++, NodeJS, Perl, Rust
+sudo dnf -y install java-latest-openjdk-devel cmake meson binutils libtool glibc-devel \
     gcc-c++ clang nodejs npm perl
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -- -y
 
 # Useful Python packages
-sudo dnf install poetry python3-{requests,beautifulsoup4,gobject}
+sudo dnf -y install poetry python3-{requests,beautifulsoup4,gobject}
+
+# Google Chrome
+sudo dnf -y config-manager --set-enabled google-chrome
+sudo dnf -y check-update
+sudo dnf -y install google-chrome-stable
 
 # VS Code https://code.visualstudio.com/docs/setup/linux
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf check-update
-sudo dnf install code
+sudo dnf -y check-update
+sudo dnf -y install code
 
 # 1Password Beta https://support.1password.com/betas
 sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
-sudo sudo sh -c 'echo -e "[1password-beta]\nname=1Password Beta Channel\nbaseurl=https://downloads.1password.com/linux/rpm/beta/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password-beta.repo'
+sudo sh -c 'echo -e "[1password-beta]\nname=1Password Beta Channel\nbaseurl=https://downloads.1password.com/linux/rpm/beta/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password-beta.repo'
 sudo dnf check-update
 sudo dnf install 1password 1password-cli
 
 # NVIDIA Container Toolkit https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html
 curl -s -L https://nvidia.github.io/libnvidia-container/rhel9.0/libnvidia-container.repo | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo # As of November 2022
 unset distribution
-sudo dnf check-update
-sudo dnf install nvidia-container-toolkit
+sudo dnf -y check-update
+sudo dnf -y install nvidia-container-toolkit
 sudo sed -i 's/^#no-cgroups = false/no-cgroups = true/;' /etc/nvidia-container-runtime/config.toml # rootless
 
 # Other tools
-sudo dnf install fakeroot gh dconf-editor screen podman podman compose buildah skopeo
-sudo flatpak install net.werwolv.ImHex
+sudo dnf -y install fakeroot flatpak-builder gh dconf-editor screen podman podman compose buildah skopeo
+sudo flatpak install --noninteractive net.werwolv.ImHex
 
 # Environment setup
-pip install git+ssh://git@github.com/powerline/powerline.git@develop # pip is out of date, see powerline#2116
-sudo dnf install vim-enhanced fira-code-fonts kitty neofetch powerline-fonts
+yes | pip install git+ssh://git@github.com/powerline/powerline.git@develop # pip is out of date, see powerline#2116
+sudo dnf -y install vim-enhanced fira-code-fonts kitty neofetch powerline-fonts
 gsettings set org.gnome.nautilus.preferences show-hidden-files true
 gsettings set org.gtk.gtk4.Settings.FileChooser show-hidden true
 
