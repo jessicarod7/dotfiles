386413cf0d45e93905ca574255e2538e65230f0a
Cameron Rodriguez <carodrig@redhat.com>
2023-01-18 18:18:08 -0500

    Update expressvpn-upgrade argument parser, better bashrc $HOME usage

--- bash_kittyterm/.bashrc
+++ bash_kittyterm/.bashrc
@@ -2,7 +2,6 @@
 
 ### ...set user PATH ###
 
-export PATH="$PATH:$HOME/scripts" # For all the scripting fun
 source <(op completion bash)
 alias chrome=google-chrome-stable
 alias lso='ls -lash --time-style=long-iso'
@@ -10,13 +9,15 @@ alias ssk='kitty +kitten ssh'
 alias qalc='flatpak run --command=qalc io.github.Qalculate'
 
 # Scripts
-alias colocat='python3 ~/scripts/colocat.py'
-alias colodiff='python3 ~/scripts/colodiff.py'
-alias git-unsync='python3 ~/scripts/git-unsync.py'
+export PATH="$PATH:$HOME/scripts" # For all the scripting fun
+alias colocat="python3 $HOME/scripts/colocat.py"
+alias colodiff="python3 $HOME/scripts/colodiff.py"
+alias expressvpn-upgrade="sudo python $HOME/scripts/expressvpn-upgrade.py"
+alias git-unsync="python3 $HOME/scripts/git-unsync.py"
 
 export EDITOR='/usr/bin/vim -e'
 export VISUAL=/usr/bin/vim
-export KITTY_CUSTOM=~/.config/kitty/kitty.d
+export KITTY_CUSTOM="$HOME/.config/kitty/kitty.d"
 
 # Add Java home, IntelliJ to PATH
 export JAVA_HOME=/usr/lib/jvm/java
@@ -36,7 +37,7 @@ if [ -f `which powerline-daemon` ]; then
 	powerline-daemon -q
 	POWERLINE_BASH_CONTINUATION=1
 	POWERLINE_BASH_SELECT=1
-	. ~/.local/lib/python3.11/site-packages/powerline/bindings/bash/powerline.sh
+	. $HOME/.local/lib/python3.11/site-packages/powerline/bindings/bash/powerline.sh
 fi
 
 neofetch  --title_fqdn on --distro_shorthand on --refresh_rate on --gtk2 off --gtk3 off --memory_percent on # --gpu_type dedicated
--- linux_pkgs/expressvpn-upgrade.py
+++ linux_pkgs/expressvpn-upgrade.py
@@ -15,35 +15,26 @@
 # - packaging (included with setuptools, pip3 install packaging)
 
 
-from urllib.request import urlopen
+from argparse import ArgumentParser, Namespace
 from bs4 import BeautifulSoup
 from packaging.version import Version
+from urllib.request import urlopen
 import subprocess
 import sys
 
-def help() -> None:
-	print("ExpressVPN Upgrader and Installer\n")
-	print("==================================\n")
-	print("This script is best used with the script `expressvpn-upgrade` as a caller.\n")
-	print("Usage: expressvpn-upgrade [--install] [--no-gpg-check]\n")
-	print("Options:\n")
-	print("\t--install: Install ExpressVPN instead of upgrading\n")
-	print("\t--no-gpg-check: Do not check if the signature of the downloaded package matches the ExpressVPN\n" + \
-		  "\t                release key (see https://www.expressvpn.com/support/vpn-setup/pgp-for-linux/)\n")
-
-def checkVersion(install_option: bool=False) -> Version:
+def checkVersion(installing: bool) -> Version:
 	try:
 		versionS = subprocess.check_output(['expressvpn', '--version'])
 		versionL = versionS.split()
 		return Version(versionL[2].decode('utf-8'))
 	except:
-		if install_option:
+		if installing:
 			return Version("0")
-		
-		print("Invalid option: ExpressVPN is not installed. Use the --install option.\n")
-		quit()
+		else:
+			print("Invalid option: ExpressVPN is not installed. Use the --install option.\n")
+			quit()
 
-def downloadAndVerifyRPM(newVersion: Version, rpmURL: str, rpmSigURL: str) -> str:
+def downloadAndVerifyRPM(args: Namespace, newVersion: Version, rpmURL: str, rpmSigURL: str) -> str:
 	rpmFile = "/tmp/expressvpn-" + str(newVersion) + ".rpm"
 	rpmStream = urlopen(rpmURL)
 	with open(rpmFile, 'b+w') as rpmFileStream:
@@ -55,7 +46,7 @@ def downloadAndVerifyRPM(newVersion: Version, rpmURL: str, rpmSigURL: str) -> st
 		sigFileStream.write(sigStream.read())
 
 	# Verify
-	if '--no-gpg-check' not in sys.argv:
+	if not args.no_gpg_check:
 		if (subprocess.call(["gpg", "--verify", rpmSigFile, rpmFile], stdout=subprocess.DEVNULL) != 0):
 			print("ERROR: Package signature could not be verified. Please check that the ExpressVPN PGP key is added to root.\n", file=sys.stderr)
 			print("Alternatively, run with '--no-gpg-check' to suppress this check. Cancelling upgrade.\n", file=sys.stderr)
@@ -66,8 +57,8 @@ def downloadAndVerifyRPM(newVersion: Version, rpmURL: str, rpmSigURL: str) -> st
 
 	return rpmFile
 
-def upgrade(newVersion: Version, rpmURL: str, rpmSigURL: str) -> None:
-	currVersion = checkVersion()
+def upgrade(args: Namespace, newVersion: Version, rpmURL: str, rpmSigURL: str) -> None:
+	currVersion = checkVersion(args.install)
 
 	if currVersion < newVersion:
 		while True:
@@ -75,7 +66,7 @@ def upgrade(newVersion: Version, rpmURL: str, rpmSigURL: str) -> None:
 			p = input("Install? [y/N] ")
 			if p.lower() == 'y':
 				print("\nUpgrading ExpressVPN...\n")
-				rpmFile = downloadAndVerifyRPM(newVersion, rpmURL, rpmSigURL)
+				rpmFile = downloadAndVerifyRPM(args, newVersion, rpmURL, rpmSigURL)
 
 				subprocess.call("sudo dnf install " + rpmFile)
 				sys.exit()
@@ -89,15 +80,15 @@ def upgrade(newVersion: Version, rpmURL: str, rpmSigURL: str) -> None:
 		print("ERROR: Latest version (" + str(newVersion) + ") is lower than installed version (" + str(currVersion) + "). Cancelling upgrade.\n", file=sys.stderr)
 		sys.exit(3)
 
-def install(newVersion: Version, rpmURL: str, rpmSigURL: str) -> None:
-	if checkVersion(True) is not Version("0"):
+def install(args: Namespace, newVersion: Version, rpmURL: str, rpmSigURL: str) -> None:
+	if checkVersion(args.install) != Version("0"):
 		print("Invalid option: ExpressVPN is already installed. Do not use the --install option.\n")
 		sys.exit(3)
 
 	p = input(f"Confirm installation of ExpressVPN {str(newVersion)} [y/N]: ")
 	if p.lower() == 'y':
 		print("\nInstalling ExpressVPN...")
-		rpmFile = downloadAndVerifyRPM(newVersion, rpmURL, rpmSigURL)
+		rpmFile = downloadAndVerifyRPM(args, newVersion, rpmURL, rpmSigURL)
 		
 		subprocess.call("sudo dnf install " + rpmFile)
 		sys.exit()
@@ -106,10 +97,15 @@ def install(newVersion: Version, rpmURL: str, rpmSigURL: str) -> None:
 		sys.exit(1)
 
 if __name__ == "__main__":
-	if '--help' in sys.argv:
-		help()
+	parser = ArgumentParser(prog="ExpressVPN Upgrader and Installer", description="A small script to upgrade (or install) Fedora x86_64 versions of ExpressVPN")
+	parser.add_argument("-i", "--install", help="Install the latest version of ExpressVPN on a device that doesn't already have it",
+						action="store_true")
+	parser.add_argument("-nogpg", "--no-gpg-check", help="Do not check if the signature of the downloaded package matches the ExpressVPN " + \
+		  "release key (see https://www.expressvpn.com/support/vpn-setup/pgp-for-linux/)", action='store_false')
+	args: Namespace = parser.parse_args()
 
-	url = "https://www.expressvpn.com/latest#linux?utm_source=linux_app"
+
+	url: str = "https://www.expressvpn.com/latest#linux?utm_source=linux_app"
 	page = urlopen(url)
 	soup = BeautifulSoup(page, "html.parser")
 	linkTag = soup.find_all("option", text = "Fedora 64-bit")
@@ -119,4 +115,4 @@ if __name__ == "__main__":
 	newVersion = Version(tag['data-version'])
 	rpmSigURL = rpmURL + ".asc"
 
-	install(newVersion, rpmURL, rpmSigURL) if '--install' in sys.argv else upgrade(newVersion, rpmURL, rpmSigURL)
+	install(args, newVersion, rpmURL, rpmSigURL) if args.install else upgrade(args, newVersion, rpmURL, rpmSigURL)
