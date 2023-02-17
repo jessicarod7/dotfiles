b076ffb3a051f12281eb9122f5a5244eb239bb21
Cameron Rodriguez <dev@camrod.me>
2023-02-17 13:43:46 -0500

    better Zeal install setup

--- zeal/zeal_setup.sh
+++ zeal/zeal_setup.sh
@@ -6,12 +6,11 @@ else
 fi
 podman run -dt --name zeal-build zeal-builder:latest
 sudo mkdir /opt/zeal
-sudo chown -R camrod:camrod /opt/zeal
+sudo chown -R <uid>:<gid> /opt/zeal
 sudo chmod -R ug+w /opt/zeal
-podman cp zeal-build:/export /opt/zeal
+podman cp zeal-build:/export/. /opt/zeal
 podman stop zeal-build && podman rm zeal-build
 
-mv /opt/zeal/export/* /opt/zeal && rmdir /opt/zeal/export
 mkdir /opt/zeal/feed_srcs
 ln -s /opt/zeal/share/applications/org.zealdocs.zeal.desktop ~/.local/share/applications/org.zealdocs.zeal.desktop
 sudo dnf install qt5-qtwebengine
