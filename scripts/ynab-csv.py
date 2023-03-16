da501fcae0f1f779faad240dcdc8244c0a566560
Cameron Rodriguez <dev@camrod.me>
2023-03-15 20:45:19 -0400

    remove wildscribe docset, update ynab-csv

--- scripts/ynab-csv.py
+++ scripts/ynab-csv.py
@@ -1,39 +1,62 @@
 # Convert CSVs into YNAB format for import
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from datetime import datetime
+from pathlib import Path
 
 import csv, sys
 
-parser = ArgumentParser(description="A script to convert bank CSVs to YNAB format. Outputs as <file>_mapped.csv")
+parser = ArgumentParser(description="A script to convert bank CSVs to YNAB format. Outputs as <file>_mapped.csv", \
+                            formatter_class=ArgumentDefaultsHelpFormatter)
 parser.add_argument("file", help="CSV file to convert");
-parser.add_argument("-b", "--bank", help="The bank to convert from. Defaults to 'e'.", default="e")
+parser.add_argument("-b", "--bank", help="The bank/format to convert from.", default="e", choices=['e', 'ecard'])
 args = parser.parse_args()
 
-if args.bank == 'e': # Columns are Date,Description,Transfer,Balance
-    with open(args.file, newline='') as raw_csvfile:
-        csvfile = csv.DictReader(raw_csvfile)
+match args.bank:
+    case 'e': # Columns are Date,Description,Transfer,Balance
+        with open(args.file, newline='') as raw_csvfile:
+            csvfile = csv.DictReader(raw_csvfile)
 
-        with open(args.file[:-4]+"_mapped.csv", newline='', mode="w") as new_rawfile:
-            newfile = csv.DictWriter(new_rawfile, fieldnames=["Date", "Payee", "Memo", "Amount"])
-            newfile.writeheader()
+            with open(Path(args.file).with_suffix('').__str__() +"_mapped.csv", \
+                        newline='', mode="w") as new_rawfile:
+                newfile = csv.DictWriter(new_rawfile, fieldnames=["Date", "Payee", "Memo", "Amount"])
+                newfile.writeheader()
 
-            for row in csvfile:
-                # Convert date from DD MMM YYYY to ISO 8601
-                new_date = "{0:%Y}-{0:%m}-{0:%d}".format(datetime.strptime(row["Date"], "%d %b %Y"))
+                for row in csvfile:
+                    # Convert date from DD MMM YYYY to ISO 8601
+                    new_date = "{0:%Y}-{0:%m}-{0:%d}".format(datetime.strptime(row["Date"], "%d %b %Y"))
 
-                # Try to guess payee
-                desc = row["Description"]
-                if "to " in desc:
-                    payee = desc[desc.find("to ")+3:].lstrip()
-                elif "from " in desc:
-                    payee = desc[desc.find("from ")+5:].lstrip()
-                else:
-                    payee = desc
+                    # Try to guess payee
+                    desc = row["Description"]
+                    if "to " in desc:
+                        payee = desc[desc.find("to ")+3:].lstrip()
+                    elif "from " in desc:
+                        payee = desc[desc.find("from ")+5:].lstrip()
+                    else:
+                        payee = desc
 
-                # Format amount to numeric
-                amount = "{:.2f}".format(float(row["Transfer"].replace('$','')))
+                    # Format amount to numeric
+                    amount = "{:.2f}".format(float(row["Transfer"].replace('$','')))
 
-                newfile.writerow({"Date": new_date, "Payee": payee, "Memo": row["Description"], "Amount": amount})
+                    newfile.writerow({"Date": new_date, "Payee": payee, "Memo": row["Description"], "Amount": amount})
+    case 'ecard': # Columns are Date,Description,Amount
+        with open(args.file, newline='') as raw_csvfile:
+            csvfile = csv.DictReader(raw_csvfile)
 
-else:
-    print("Other banks are not supported, sorry.",file=sys.stderr)
+            with open(Path(args.file).with_suffix('').__str__() +"_mapped.csv", \
+                        newline='', mode="w") as new_rawfile:
+                newfile = csv.DictWriter(new_rawfile, fieldnames=["Date", "Payee", "Memo", "Amount"])
+                newfile.writeheader()
+
+                for row in csvfile:
+                    # Convert date from DD MMM YYYY to ISO 8601
+                    new_date = "{0:%Y}-{0:%m}-{0:%d}".format(datetime.strptime(row["Date"], "%d %b %Y"))
+
+                    # Try to guess payee
+                    payee = row["Description"].split(',')[0]
+
+                    # Format amount to numeric
+                    amount = "{:.2f}".format(float(row["Amount"].replace('$','')))
+
+                    newfile.writerow({"Date": new_date, "Payee": payee, "Memo": row["Description"], "Amount": amount})
+    case _:
+        print("Other banks are not supported, sorry.",file=sys.stderr)
--- zeal/docsets.md
+++ zeal/docsets.md
@@ -3,5 +3,3 @@
 - From [User Contributions repo](https://github.com/Kapeli/Dash-User-Contributions) via [this website](https://zealusercontributions.vercel.app/)
   - [JUnit 4](https://zealusercontributions.vercel.app/api/docsets/JUnit4.xml)
   - [Jakarta EE 10 (by me!)](https://zealusercontributions.vercel.app/api/docsets/Jakarta_EE_10.xml)
-- Using [godbout/dash-docset-builder](https://github.com/godbout/dash-docset-builder)
-  - Custom docset for [WildFly documentation](https://docs.wildfly.org) and [model reference (Wildscribe)](https://docs.wildfly.org/27/wildscribe)
