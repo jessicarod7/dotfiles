665225cff0340d9d13f0a456967eb04dba6431ab
Cameron Rodriguez <dev@camrod.me>
2023-04-03 20:51:11 -0400

    update colodiff to handle matching files lol

--- scripts/colodiff.py
+++ scripts/colodiff.py
@@ -21,19 +21,23 @@ def run_cmd(cmd: list[str]) -> sp.CompletedProcess:
     return output
 
 def diff_files(args: Namespace):
-    output: list[str] = run_cmd(["diff", "-u", args.file1, args.file2]).stdout.splitlines()
+    cmd_result = run_cmd(["diff", "-u", args.file1, args.file2])
+    output: list[str] = cmd_result.stdout.splitlines()
 
-    print(BOLD + output[0] + '\n' + output[1] + RESET)
-    del(output[0:2])
-    for line in output:
-        if (line[0] == '-'):
-            print(RED + line + RESET)
-        elif (line[0] == '+'):
-            print(GREEN + line + RESET)
-        elif (line[0] == '@'):
-            print(BLUE + line + RESET)
-        else:
-            print(line)
+    if len(output) > 0:
+        print(BOLD + output[0] + '\n' + output[1] + RESET)
+        del(output[0:2])
+        for line in output:
+            if (line[0] == '-'):
+                print(RED + line + RESET)
+            elif (line[0] == '+'):
+                print(GREEN + line + RESET)
+            elif (line[0] == '@'):
+                print(BLUE + line + RESET)
+            else:
+                print(line)
+
+    sys.exit(cmd_result.returncode)
 
 if __name__ == "__main__":
     parser = ArgumentParser(prog='colodiff', description="diff but with color for containers that don't have modern diffutils")
