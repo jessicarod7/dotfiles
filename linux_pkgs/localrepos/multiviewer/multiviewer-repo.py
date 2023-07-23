565558eacd7b2284481be1fbe528eaeec6924d04
Cameron Rodriguez <dev@camrod.me>
2023-07-23 09:44:27 -0400

    switch to selenium for multiviewer-for-f1 scraper

--- linux_pkgs/localrepos/multiviewer/multiviewer-repo.py
+++ linux_pkgs/localrepos/multiviewer/multiviewer-repo.py
@@ -7,22 +7,29 @@ import subprocess
 import sys
 from pathlib import Path
 
-from bs4 import BeautifulSoup, Tag
 import requests
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 LOCALREPO = Path("~/.local/share/localrepos/multiviewer/x86_64/").expanduser().resolve()
 
 
 def get_rpm_url(version: str) -> str | None:
-    rpm_url: str = ""
+    cr_opt = ChromeOptions()
+    cr_opt.add_argument("--headless=new")
+    dl_page = Chrome(options=cr_opt)
 
-    dl_page = BeautifulSoup(requests.get("https://multiviewer.app/download").text, 'html.parser')
-    for anchor in dl_page.find_all("a"):
-        if isinstance(anchor, Tag):
-            if any('linux_rpm' in attribute for attribute in list(anchor.attrs.values())):
-                rpm_url = anchor.get("href")
+    dl_page.get("https://multiviewer.app/download")
+    WebDriverWait(dl_page, timeout=15).until(
+        lambda d: d.find_element(by=By.PARTIAL_LINK_TEXT, value=version)
+    )
 
-    return rpm_url if version in rpm_url else None
+    anchor = dl_page.find_element(by=By.XPATH, value='//a[@data-platform="linux_rpm"]')
+    rpm_url = anchor.get_attribute("href")
+    dl_page.quit()
+
+    return rpm_url if rpm_url is None else (rpm_url if version in rpm_url else None)
 
 
 releases = json.loads(requests.get("https://api.multiviewer.dev/api/v1/releases").text)
@@ -46,7 +53,9 @@ if not any(latest_dl in f for f in os.listdir(LOCALREPO)):
         subprocess.run(["createrepo_c", LOCALREPO], check=True)
         print("Successfully registered to local repo. Update can be installed via DNF.")
     else:
-        print(f"Unable to retrieve RPM for {latest_dl} from multiviewer.app/download -- repo not updated")
+        print(
+            f"Unable to retrieve RPM for {latest_dl} from multiviewer.app/download -- repo not updated"
+        )
         sys.exit(1)
 else:
     print(f"No updates to MultiViewer for F1(R). Current release is {latest_dl}.")
