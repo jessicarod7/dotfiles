c4e4c39d58500b37d8655cc1df2f0dd4cdf7d9a2
Cameron Rodriguez <dev@camrod.me>
2023-08-26 11:22:29 -0400

    fix duplicate names for zoom updater

--- linux_pkgs/localrepos/zoom/zoom-repo.py
+++ linux_pkgs/localrepos/zoom/zoom-repo.py
@@ -2,35 +2,61 @@
 # Requires requests
 import subprocess
 
-import requests
 import os
 from pathlib import Path
 import re
 import sys
 
-LOCALREPO = Path("~/.local/share/localrepos/zoom/x86_64/").expanduser().resolve()
+import requests
 
-resp = requests.get("https://zoom.us/client/latest/zoom_x86_64.rpm", allow_redirects=False)
+LOCALREPO = Path("~/.local/share/localrepos/zoom/x86_64/").expanduser().resolve()
+INIT_RPM_NAME = "newzoom.rpm"
+
+resp = requests.get(
+    "https://zoom.us/client/latest/zoom_x86_64.rpm", allow_redirects=False
+)
 latest_dl: str = ""
-if resp.status_code == 302 and (redirect := resp.headers.get('location')) is not None:
-    vers = re.search(r'/(\d+(\.\d+?)*)/', redirect)
+if resp.status_code == 302 and (redirect := resp.headers.get("location")) is not None:
+    vers = re.search(r"/(\d+(\.\d+?)*)/", redirect)
     if vers is not None:
         latest_dl = vers.group(1) if len(vers.groups()) >= 0 else ""
 if latest_dl == "":
-    print(f"Unable to retrieve latest Zoom version. Output of request:\n {resp}\nBody: {resp.text}\nHeaders: {resp.headers}", file=sys.stderr)
+    print(
+        f"Unable to retrieve latest Zoom version. Output of request:\n {resp}\nBody: {resp.text}\nHeaders: {resp.headers}",
+        file=sys.stderr,
+    )
     sys.exit(1)
 
 if not any(latest_dl in f for f in os.listdir(LOCALREPO)):
+    rpm_url = f"https://zoom.us/client/{latest_dl}/zoom_x86_64.rpm"
+
     print(f"Downloading Zoom {latest_dl}...")
     subprocess.run(
         [
             "wget",
-            f"https://zoom.us/client/{latest_dl}/zoom_x86_64.rpm",
-            "--directory-prefix",
-            LOCALREPO,
+            "-O",
+            LOCALREPO.joinpath(INIT_RPM_NAME),
+            rpm_url,
         ],
         check=True,
     )
+    rpm_name = (
+        subprocess.check_output(
+            [
+                "rpm",
+                "-q",
+                "--queryformat",
+                "%{NEVRA}",
+                LOCALREPO.joinpath(INIT_RPM_NAME),
+            ]
+        ).decode("ascii")
+        + ".rpm"
+    )
+    subprocess.run(
+        ["mv", LOCALREPO.joinpath(INIT_RPM_NAME), LOCALREPO.joinpath(rpm_name)],
+        check=True,
+    )
+
     subprocess.run(["createrepo_c", LOCALREPO], check=True)
     print("Successfully registered to local repo. Update can be installed via DNF.")
 else:
