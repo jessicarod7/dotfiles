bbcb658dd01935031e2b2099aec238bac4e1a2d3
Cameron Rodriguez <dev@camrod.me>
2023-09-03 02:06:36 -0400

    increase DNF max_parallel_downloads to 15

--- linux_pkgs/devtools.sh
+++ linux_pkgs/devtools.sh
@@ -6,7 +6,7 @@ if [[ ! "$(dirname $(pwd))" =~ "/linux_pkgs" ]]; then
 fi
 
 # RPM Fusion, other nonfree libraries, and first updates
-echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
+echo 'max_parallel_downloads=15' | sudo tee -a /etc/dnf/dnf.conf
 sudo dnf -y check-update
 sudo dnf -y upgrade
 sudo dnf -y install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
