563e4a670d1f864aa5d58a88fb9031c9b2e060ca
Cameron Rodriguez <dev@camrod.me>
2024-08-17 09:11:33 -0400

    add kwrite alias, update git lg/alog fmt

--- bash_kittyterm/bashrc.d/aliases.sh
+++ bash_kittyterm/bashrc.d/aliases.sh
@@ -7,3 +7,4 @@ alias tarZ="tar -I'zstd -T0'"
 # shellcheck disable=SC2139
 alias colocat="python3 $HOME/scripts/colocat.py"
 alias rsyncopy="rsync -avz --info=progress2"
+alias kwrite="flatpak run org.kde.kwrite"
