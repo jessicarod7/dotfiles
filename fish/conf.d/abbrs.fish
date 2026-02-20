aff266ab3b47cf47827b26d5efcf1c7cde8ec408
Jessica Rodriguez <dev@jessicarod.com>
2026-02-20 08:26:31 -0500

    replace ssk abbr for kitty-term

--- fish/conf.d/abbrs.fish
+++ fish/conf.d/abbrs.fish
@@ -4,6 +4,6 @@ abbr -a tarZ -r 'tar[Zz]' tar -I'"zstd -T0"'
 abbr -a rsyncopy rsync -avz --info=progress2
 abbr -a colordiff diff -u --color
 
-abbr -a ssk kitty +kitten ssh
+abbr -a ssk kitten ssh
 abbr -a chrome google-chrome-stable
 abbr -a kwrite flatpak run org.kde.kwrite
